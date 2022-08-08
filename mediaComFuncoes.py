9a3de2526d83c4334652b26e0ef9b3f28c03adc2
Felype Nunes de Souza <101263522+felps2003@users.noreply.github.com>
2022-08-08 20:37:14 -0300

    Add files via upload

--- mediaComFuncoes.py
+++ mediaComFuncoes.py
@@ -17,15 +17,78 @@ def totalGlobal(gs, sp, ch):
 def notaFinal(med1, med2):
     return (med1*0.4)+(med2*0.6)
 
-ch1 = checkpoint(50.0,50.0,20.0)
-sp1 = Sprint(70.0,90.0)
-md1 = totalGlobal(100.0, sp1, ch1 )
+def pedirNota():
+    x = float(input("Digite a nota: "))
+    return x
 
-ch2 = checkpoint(50.0,50.0,20.0)
-sp2 = Sprint(70.0,90.0)
-md2 = totalGlobal(50.0, sp1, ch1 )
+# ======== /\ funções =========== \/ codigo ==========
 
-print(f"Sua nota final é: {notaFinal(md1,md2)}")
+print(f"============== Calcule o primeiro semestre ================")
+print("---------------- Digite as 3 notas dos CheckPoints ------------------")
+ch1 = pedirNota()
+ch2 = pedirNota()
+ch3 = pedirNota()
 
+ch0 = checkpoint(ch1,ch2,ch3)
+print("=================================================================")
+print("---------------- Digite as 2 notas das Sprints ------------------")
+sp1 = pedirNota()
+sp2 = pedirNota()
+
+sp0 = Sprint(sp1,sp2)
+print("=================================================================")
+print("---------------- Digite a nota do Global Solution ------------------")
+md1 = pedirNota()
+
+md0 = totalGlobal(md1, sp0, ch0 )
+print("=================================================================")
+print(f"---------------- Sua media no primeiro semestre é {md0} ------------------")
+
+print("=================================================================")
+print(f"================== Calcule o segundo semestre ====================")
+print("---------------- Digite as 3 notas dos CheckPoints ------------------")
+ch1_1 = pedirNota()
+ch2_1 = pedirNota()
+ch3_1 = pedirNota()
+
+ch0_1 = checkpoint(ch1_1,ch2_1,ch3_1)
+
+print("=================================================================")
+print("---------------- Digite as 2 notas das Sprints ------------------")
+sp1_1 = pedirNota()
+sp2_1 = pedirNota()
+
+sp0_1 = Sprint(sp1_1,sp2_1)
+
+print("=================================================================")
+print("---------------- Digite a nota do Global Solution ------------------")
+md1_1 = pedirNota()
+
+md0_1 = totalGlobal(md1_1, sp0_1, ch0_1 )
+
+print("=================================================================")
+print(f"---------------- Sua media no segundo semestre é {md0_1} ------------------")
+
+n = ((md0*0.4)+(md0_1*0.6))
+
+if n >= 60:
+    print("=================================================================")
+    print(f"       Parabéns você foi aprovado, sua media final é {n}  =)")
+    print("=================================================================")
+elif n >= 40:
+    print("Você tera que realizar outro exame para conseguir passar de ano")
+    novaNota = float(input("Qual nota você conseguiu nesse exame ?"))
+    if novaNota >= 60:
+        print("=================================================================")
+        print("                Parabéns você conseguiu passar! =)")
+        print("=================================================================")
+    else:
+        print("=================================================================")
+        print("               Infelizmente você não conseguiu passar! =(")
+        print("=================================================================")
+else:
+    print("=================================================================")
+    print("               Infelizmente você não foi aprovado =(")
+    print("=================================================================")
 
 
